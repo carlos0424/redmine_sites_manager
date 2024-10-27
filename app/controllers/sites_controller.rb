7faeac07e9c00303581fb238cc1e09290336f14b
Carlos <carlosarbelaez0424@hotmail.com>
2024-10-27 12:53:49 -0500

    Errores

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -186,6 +186,10 @@ class SitesController < ApplicationController
       :campo_adicional_4, :campo_adicional_5
     )
   end
+  def get_coordinators
+    coordinators = User.where(role: 'Coordinador').select(:id, :name)
+    render json: coordinators.map { |user| { id: user.id, name: user.name } }
+  end
 
   def search_sites(term)
     FlmSite.where(
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ RedmineApp::Application.routes.draw do
   resources :sites do
     collection do
       get 'search', :to => 'sites#search'  
+      get 'get_coordinators', to: 'users#get_coordinators'
       post 'import'
       get 'download_template'
       post 'bulk_update'
