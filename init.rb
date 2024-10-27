23dc414a9cf19991e652764bae4b0767a3e425fd
Carlos <carlosarbelaez0424@hotmail.com>
2024-10-27 01:32:12 -0500

    Errores

--- init.rb
+++ init.rb
@@ -1,14 +1,18 @@
+require 'redmine'
+
 Redmine::Plugin.register :redmine_sites_manager do
-  name I18n.t('plugin_sites_manager.name')
+  name I18n.t('plugin_sites_manager.name') # Llamada directa a I18n.t para cargar el nombre traducido
   author 'Carlos Arbelaez'
   description 'Plugin para gestión y búsqueda de sitios FLM'
   version '1.0.0'
 
+  # Menú en la administración que apunta directamente a la configuración del plugin
   menu :admin_menu, :sites_manager, 
        { controller: 'settings', action: 'plugin', id: 'redmine_sites_manager' },
-       caption: I18n.t('plugin_sites_manager.name'),
+       caption: I18n.t('plugin_sites_manager.name'), # Uso directo de I18n.t para cargar el nombre
        html: { class: 'icon icon-package' }
-       
+
+  # Configuración del plugin
   settings default: {
     'custom_fields_mapping' => {
       'sitio' => '',
