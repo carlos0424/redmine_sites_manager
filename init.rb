b49c9634ab06df896417826edc7cdda153e410ab
Carlos <carlosarbelaez0424@hotmail.com>
2024-10-27 10:34:25 -0500

    Errores

--- init.rb
+++ init.rb
@@ -1,23 +1,21 @@
 require 'redmine'
-require_dependency File.expand_path('redmine_sites_manager/hooks', __dir__)  # Cargar hooks con ruta absoluta para evitar errores de carga
-
-# Agregar archivos de localización para el plugin
 I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'config', 'locales', '*.yml')]
-I18n.default_locale = :es  # Forzar el idioma español para Redmine
+
+# Forzar el uso del idioma español en Redmine
+I18n.default_locale = :es
+I18n.reload!  # Forzar la recarga de las traducciones
 
 Redmine::Plugin.register :redmine_sites_manager do
   name I18n.t('plugin_sites_manager.name', locale: :es, default: 'Gestor de Sitios FLM')
   author 'Carlos Arbelaez'
   description I18n.t('plugin_sites_manager.description', locale: :es, default: 'Plugin para gestión de sitios FLM')
   version '1.0.0'
-  
-  # Configurar el menú del administrador
+
   menu :admin_menu, :sites_manager, 
        { controller: 'settings', action: 'plugin', id: 'redmine_sites_manager' },
        caption: I18n.t('plugin_sites_manager.name', locale: :es, default: 'Gestor de Sitios FLM'),
        html: { class: 'icon icon-package' }
 
-  # Configuración por defecto del plugin
   settings default: {
     'custom_fields_mapping' => {
       'sitio' => '',
@@ -32,4 +30,5 @@ Redmine::Plugin.register :redmine_sites_manager do
       'campo_adicional_5' => ''
     }
   }, partial: 'settings/sites_manager_settings'
+  
 end
