05c720475c61f2ca04bbdec1fab8096763141fdf
Carlos <carlosarbelaez0424@hotmail.com>
2024-10-27 09:40:39 -0500

    Errores

--- lib/redmine_sites_manager/hooks.rb
+++ lib/redmine_sites_manager/hooks.rb
@@ -2,96 +2,189 @@ module RedmineSitesManager
   class Hooks < Redmine::Hook::ViewListener
     # Incluir CSS y JS en el header de todas las páginas
     def view_layouts_base_html_head(context={})
+      return unless valid_context?(context)
+      
       stylesheet_link_tag('sites_manager', plugin: 'redmine_sites_manager') +
+      javascript_include_tag('select2.min', plugin: 'redmine_sites_manager') +
       javascript_include_tag('sites_manager_admin', plugin: 'redmine_sites_manager')
     end
 
     # Hook para agregar el campo de búsqueda de sitios en el formulario de incidencias
     def view_issues_form_details_top(context={})
-    return unless context[:issue].new_record? || context[:issue].status_id.to_s == '1'
-    
-    html = <<-HTML
-      <div class="sites-search-container">
-        <label>#{l('plugin_sites_manager.sites.search_label')}</label>
-        <input type="text" id="sites-search-field" 
-               class="sites-autocomplete" 
-               placeholder="#{l('plugin_sites_manager.search.placeholder')}" />
-      </div>
-    HTML
-    
-    html.html_safe
-  end
-end
+      return '' unless show_site_search?(context[:issue])
+      
+      html = <<-HTML
+        <div class="sites-search-container">
+          <p>
+            <label>#{l('plugin_sites_manager.sites.search_label')}</label>
+            <input type="text" 
+                   id="sites-search-field" 
+                   class="sites-autocomplete" 
+                   placeholder="#{l('plugin_sites_manager.search.placeholder')}"
+                   autocomplete="off" />
+            <span class="sites-clear-btn" 
+                  title="#{l('plugin_sites_manager.sites.clear_selection')}">×</span>
+          </p>
+          <div class="sites-preview" style="display: none;">
+            <div class="selected-site-info"></div>
+          </div>
+          #{render_initialization_script}
+        </div>
+      HTML
+      
+      html.html_safe
+    end
 
     # Hook para agregar campos personalizados adicionales específicos de sitios
     def view_custom_fields_form_upper_box(context={})
-      <<-HTML
-        <p>
-          <label>#{l('plugin_sites_manager.custom_fields.site_related')}</label>
-          #{check_box_tag 'custom_field[site_related]', '1', context[:custom_field].site_related}
-        </p>
+      return '' unless context[:custom_field]
+
+      <<-HTML.html_safe
+        <div class="site-related-fields">
+          <p>
+            <label>#{l('plugin_sites_manager.custom_fields.site_related')}</label>
+            #{check_box_tag 'custom_field[site_related]', '1', 
+              context[:custom_field].site_related,
+              class: 'site-related-checkbox'}
+          </p>
+          <p class="site-field-info" style="display: none;">
+            <em class="info">#{l('plugin_sites_manager.custom_fields.site_related_info')}</em>
+          </p>
+        </div>
       HTML
     end
 
     # Hook para agregar campos personalizados en la vista de detalles de incidencia
     def view_issues_show_details_bottom(context={})
       issue = context[:issue]
-      site = FlmSite.find_by(id: issue.custom_field_value('site_id'))
+      site = find_related_site(issue)
       
-      return unless site
+      return '' unless site
+
+      render_site_details(site)
+    end
+
+    # Hook para agregar JS específico en ciertas páginas
+    def view_layouts_base_body_bottom(context={})
+      return unless should_include_js?(context)
       
-      <<-HTML
+      javascript_tag <<-JS
+        $(function() {
+          window.sitesManagerSettings = {
+            searchUrl: '#{sites_search_url}',
+            customFieldMappings: #{get_custom_field_mappings.to_json},
+            translations: #{get_translations.to_json},
+            fieldMapping: #{get_field_mapping.to_json}
+          };
+
+          // Inicializar la búsqueda si estamos en el formulario correcto
+          if ($('#sites-search-field').length) {
+            initializeSitesSearch();
+          }
+        });
+      JS
+    end
+
+    private
+
+    def valid_context?(context)
+      context[:controller] && 
+      (context[:controller].is_a?(IssuesController) || 
+       context[:controller].is_a?(SitesController))
+    end
+
+    def show_site_search?(issue)
+      issue && (issue.new_record? || issue.status_id.to_s == '1')
+    end
+
+    def sites_search_url
+      url_for(
+        controller: 'sites',
+        action: 'search',
+        format: 'json',
+        only_path: true
+      )
+    end
+
+    def get_custom_field_mappings
+      Setting.plugin_redmine_sites_manager['custom_fields_mapping'] || {}
+    end
+
+    def get_field_mapping
+      {
+        1 => 's_id',
+        5 => 'nom_sitio',
+        8 => 'identificador',
+        10 => 'depto',
+        2 => 'municipio',
+        3 => 'direccion',
+        6 => 'jerarquia_definitiva',
+        7 => 'fijo_variable',
+        9 => 'coordinador'
+      }
+    end
+
+    def get_translations
+      {
+        noResults: l('plugin_sites_manager.search.no_results'),
+        searching: l('plugin_sites_manager.search.searching'),
+        placeholder: l('plugin_sites_manager.search.placeholder'),
+        clearSelection: l('plugin_sites_manager.sites.clear_selection')
+      }
+    end
+
+    def find_related_site(issue)
+      site_id = issue.custom_field_value('site_id')
+      FlmSite.find_by(id: site_id) if site_id.present?
+    end
+
+    def render_site_details(site)
+      <<-HTML.html_safe
         <div class="sites-details">
           <hr />
           <h3>#{l('plugin_sites_manager.sites.details')}</h3>
           <div class="sites-info">
-            <p>
-              <strong>#{l('plugin_sites_manager.field_s_id')}:</strong>
-              #{h(site.s_id)}
-            </p>
-            <p>
-              <strong>#{l('plugin_sites_manager.field_nom_sitio')}:</strong>
-              #{h(site.nom_sitio)}
-            </p>
-            <p>
-              <strong>#{l('plugin_sites_manager.field_direccion')}:</strong>
-              #{h(site.direccion)}
-            </p>
-            <p>
-              <strong>#{l('plugin_sites_manager.field_municipio')}:</strong>
-              #{h(site.municipio)}
-            </p>
-            <p>
-              <strong>#{l('plugin_sites_manager.field_jerarquia')}:</strong>
-              #{h(site.jerarquia)}
-            </p>
-            <p>
-              <strong>#{l('plugin_sites_manager.field_fijo_variable')}:</strong>
-              #{h(site.fijo_variable)}
-            </p>
-            <p>
-              <strong>#{l('plugin_sites_manager.field_coordinador')}:</strong>
-              #{h(site.coordinador)}
-            </p>
+            #{render_site_field(site, 's_id')}
+            #{render_site_field(site, 'nom_sitio')}
+            #{render_site_field(site, 'direccion')}
+            #{render_site_field(site, 'municipio')}
+            #{render_site_field(site, 'jerarquia_definitiva')}
+            #{render_site_field(site, 'fijo_variable')}
+            #{render_site_field(site, 'coordinador')}
           </div>
         </div>
       HTML
     end
 
-    # Hook para agregar JS específico en ciertas páginas
-    def view_layouts_base_body_bottom(context={})
-      return unless context[:controller] && 
-                   context[:controller].is_a?(IssuesController) && 
-                   ['new', 'create', 'edit', 'update'].include?(context[:controller].action_name)
-      
-      javascript_tag <<-JS
-        $(function() {
-          window.sitesManagerSettings = {
-            searchUrl: '#{url_for(controller: 'sites', action: 'search', format: 'json')}',
-            customFieldMappings: #{Setting.plugin_redmine_sites_manager['custom_field_mappings'].to_json}
-          };
-        });
-      JS
+    def render_site_field(site, field)
+      return '' unless site.respond_to?(field)
+      value = site.send(field)
+      return '' if value.blank?
+
+      <<-HTML
+        <p>
+          <strong>#{l("plugin_sites_manager.fields.#{field}")}:</strong>
+          #{h(value)}
+        </p>
+      HTML
+    end
+
+    def render_initialization_script
+      <<-HTML
+        <script>
+          $(function() {
+            if (typeof initializeSitesSearch !== 'undefined') {
+              initializeSitesSearch();
+            }
+          });
+        </script>
+      HTML
+    end
+
+    def should_include_js?(context)
+      context[:controller] && 
+      context[:controller].is_a?(IssuesController) && 
+      ['new', 'create', 'edit', 'update'].include?(context[:controller].action_name)
     end
   end
   
